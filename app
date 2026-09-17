43fe71650c7cf09ac038217b825e39218665e1c1
Your Name <you@example.com>
2026-09-17 15:21:08 +0330

    1.0.4

---
+++ app
@@ -0,0 +1,6 @@
+SetLen(00010)
+set_cv(11)
+set_second_cv(11)
+SetLen(00110)
+push(101101)
+plus
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ pub fn Lex(source: &str) -> BitVec<u8, Msb0> {
             }
             let data: String = ch[start..i].iter().collect();
             if data == "SetLen" {
-                bits.push(true); bits.push(false); bits.push(true); bits.push(false); bits.push(false);
+                bits.push(true); bits.push(false); bits.push(false); bits.push(true); bits.push(false);
                 while i < ch.len() && ch[i] != ')' {
                         match ch[i] {
                         '0' => bits.push(false),
@@ -151,7 +151,7 @@ pub fn Lex(source: &str) -> BitVec<u8, Msb0> {
         
             }
             if data == "swap_Select_To_Last" {
-                bits.extend([false, true, true, true, false]);
+                bits.extend([false,true, true, true, false]);
                 while i < ch.len() && ch[i] != ')' {
                     match ch[i] {
                         '0' => bits.push(false),
